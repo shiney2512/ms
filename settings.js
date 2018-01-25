044bf90f964f2177c924c242360707664b8d32e5
shiney2512 <35804256+shiney2512@users.noreply.github.com>
2018-01-26 00:04:19 +1100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,30 @@
+
+//cbus ip address
+exports.cbusip = '192.168.1.20';
+
+
+//cbus project name
+exports.cbusname = "MS";
+
+//mqtt server ip:port
+exports.mqtt = '127.0.0.1:1883';
+
+//username and password (unncomment to use)
+//exports.mqttusername = 'user1';
+//exports.mqttpassword = 'password1';
+
+// net and app for automatically requesting values
+// exports.getallnetapp = '254/56';
+
+// whether to request on start (requires getallnetapp set as well)
+// exports.getallonstart = true;
+
+// how often to request after start (in seconds), (requires getallnetapp set as well)
+// exports.getallperiod = 60*60;
+
+// Sets MQTT retain flag for values coming from cgate
+// exports.retainreads = true;
+
+
+//logging
+exports.logging = false;
